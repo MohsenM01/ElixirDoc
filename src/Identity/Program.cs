d5d5e17d5b443133727d30599167fb6317ef36f4
mehrabhabibi <mehrab.habibi@gmail.com>
2023-04-25 21:36:57 +0430

    FEATURE: Add athentication

--- src/Identity/Program.cs
+++ src/Identity/Program.cs
@@ -1,6 +1,27 @@
+using Microsoft.Identity.Web;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthorizationBuilder()
+.AddPolicy("admin_greetings", policy =>
+ policy
+    .RequireRole("admin")
+    .RequireScope("greetings_api"));
+
+//builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/hello", () => "Hello world!")
+.RequireAuthorization("admin_greetings");
 
 app.Run();
+
+
+
