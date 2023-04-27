af7332f9b619ebdb7694731b77b6056daf80bcb3
Farzad Ostovan <farzad_it2006@outlook.com>
2023-04-27 19:54:41 +0430

    add indentity and Swagger feature

--- src/Identity/Program.cs
+++ src/Identity/Program.cs
@@ -1,6 +1,33 @@
+using Microsoft.Identity.Web;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication().AddJwtBearer();
+//builder.Services.AddAuthorization();
+
+//builder.Services.AddAuthentication()
+//  .AddJwtBearer()
+//  .AddJwtBearer("LocalAuthIssuer");
+
+builder.Services.AddAuthorizationBuilder()
+  .AddPolicy("admin_greetings", policy =>
+        policy
+            .RequireRole("admin")
+            .RequireScope("greetings_api"));
+
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/hello", () => "Hello world!")
+  .RequireAuthorization("admin_greetings");
 
 app.Run();
