5221640669b7de9a7db568e1026dc3281312c47e
mehrabhabibi <mehrab.habibi@gmail.com>
2023-04-28 17:54:49 +0430

    FEATURE: Add Identity

--- src/Identity/Program.cs
+++ src/Identity/Program.cs
@@ -1,6 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+
+
+var connectionString = builder.Configuration.GetConnectionString("AppIdentityDbContext");
+
+builder.Services.AddDbContext<AppIdentityDbContext>(o => o.UseSqlServer(connectionString));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
