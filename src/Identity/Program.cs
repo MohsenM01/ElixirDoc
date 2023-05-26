3c38487c0d69c4ebb54fb01880b9ae2c49a2e027
mehrabhabibi <mehrab.habibi@gmail.com>
2023-05-26 16:52:30 +0330

    Add serilog in order to see logs in elastic.

--- src/Identity/Program.cs
+++ src/Identity/Program.cs
@@ -1,12 +1,24 @@
-using NLog;
-using NLog.Web;
 using Identity.Extensions;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+    {
+        DetectElasticsearchVersion = true,
+        AutoRegisterTemplate = true,
+        AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv8,
+        ModifyConnectionSettings = x => x.BasicAuthentication("elastic", "changeme"),
+        IndexFormat = "identity-{yyyy.MM.dd:0}"
+    })
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationServices(builder);
 
 builder.Logging.ClearProviders();
-builder.Host.UseNLog();
+
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 app.ConfigureApplication();
